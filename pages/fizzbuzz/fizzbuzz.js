d8d4edc0542ace0f6b65b8b2647ac13d45d16bc2
Singharato <atthaphon.singharato@dxc.com>
2022-06-24 15:50:13 +0700

    update fizzbuzz

---
+++ pages/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,27 @@
+function doSomething(){ // create fn
+    let numberInput = $('#numberFizzBuzz').val(); // get value form id html and set to {numberInput}
+    validateCondition(numberInput) // call fn and send parameter {numberInput}
+
+}
+
+
+
+
+
+function validateCondition(tempNumber) {
+    let temp_result = tempNumber % 3 // create variable for of (tempNumber) 
+   console.log(temp_result+ " temp_result") // view debug log
+
+
+    if (!temp_result) { // check temp_result  (!) **not ture 
+        $('#resultFizzBuzz').text(tempNumber +"of FIZZ")  // set text {tempNumber} of FiZZ to id {resultFizzBuzz} 
+        $('#resultDisplay').show();  // show div id {resultDisplay}
+    } else {
+        $('#resultFizzBuzz').text(tempNumber +"of NOT") // set text {tempNumber} of NOT to id {resultFizzBuzz} 
+        $('#resultDisplay').show(); // show div id {resultDisplay}
+    }
+   
+}
+
+
+
